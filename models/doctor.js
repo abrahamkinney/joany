6b4bcdc308b06e0fd159a39c83da50fc1f7e1fe1
Abraham Kinney <abraham.kinney@lootcrate.com>
2017-10-10 17:39:56 -0700

    added middleware to routes to catch all requests before passing them on

--- models/doctor.js
+++ models/doctor.js
@@ -2,7 +2,25 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var DoctorSchema   = new Schema({
-    name: String
+    name: String,
+    address: String,
+    latitude: String,
+    longitude: String,
 });
 
 module.exports = mongoose.model('Doctor', DoctorSchema);
+
+
+// doctors:
+// ● doctor_id
+// ● group_id
+// ● name
+// ● address
+// ● latitude
+// ● longitude
+// doctors_specialties:
+// ● doctor_id
+// ● specialty_id
+// specialties
+// ● specialty_id
+// ● name
